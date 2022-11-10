51dacc7cf96c33cf87ab9ddba91779086f9c10a3
Michael Thompson <thompsonmichaelj2020@gmail.com>
2022-11-10 14:20:40 -0500

    Duplicate withdrawal iterations run and tested

--- src/Simulation.java
+++ src/Simulation.java
@@ -3,7 +3,16 @@ public class Simulation {
     public void runSim(){
         System.out.println("Running simulation");
         Account myAccount = new Account(1000);
-        myAccount.withdraw(175);
+
+        for (int i = 0; i < 10; i++) {
+            myAccount.withdraw(100);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            myAccount.withdraw(100);
+        }
+
+        System.out.println("Running simulation");
     }
 
 }
