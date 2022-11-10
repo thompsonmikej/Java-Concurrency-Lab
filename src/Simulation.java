a2374cf791d112e7178a8aedbf95c58cf39c5767
Michael Thompson <thompsonmichaelj2020@gmail.com>
2022-11-10 15:38:23 -0500

    Added Runnable and String

--- src/Simulation.java
+++ src/Simulation.java
@@ -4,13 +4,17 @@ public class Simulation {
         System.out.println("Running simulation");
         Account myAccount = new Account(1000);
 
-        for (int i = 0; i < 10; i++) {
-            myAccount.withdraw(100);
-        }
+        Runnable runWithdraw = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    myAccount.withdraw(100);
+                }
+            }
+        };
 
-        for (int i = 0; i < 10; i++) {
-            myAccount.withdraw(100);
-        }
+        Thread thread1 = new Thread(runWithdraw, "Thread 1");
+        thread1.start();
 
         System.out.println("Running simulation");
     }
