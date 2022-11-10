46ac821a0c2715b2b45fc399faa0a8c4cecd558f
Michael Thompson <thompsonmichaelj2020@gmail.com>
2022-11-10 12:00:59 -0500

    First commit

---
+++ src/Simulation.java
@@ -0,0 +1,20 @@
+public class Simulation {
+
+    public void runSim(){
+        System.out.println("Running simulation");
+    }
+
+    //Instance (has a)
+
+
+
+    //Constructor
+
+
+
+    //Method (can do)
+
+
+
+
+}
